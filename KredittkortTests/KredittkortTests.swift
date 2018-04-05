569a271d5f6f81d3ad5524996e8f66733667c2be
Magdalena <magdalenabarbarapalka@gmail.com>
2018-04-05 08:43:56 +0200

    add gitignore, pods, base project

---
+++ KredittkortTests/KredittkortTests.swift
@@ -0,0 +1,36 @@
+//
+//  KredittkortTests.swift
+//  KredittkortTests
+//
+//  Created by Magdusz on 05.04.2018.
+//  Copyright © 2018 com.mcpusz.kredittkort. All rights reserved.
+//
+
+import XCTest
+@testable import Kredittkort
+
+class KredittkortTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+target 'Kredittkort' do
+target 'KredittkortTests' do
+    inherit! :search_paths
+  use_frameworks!
+    pod 'RxSwift'
+    pod 'RxCocoa'
+    pod 'SwiftLint'
+  end
+
+end
